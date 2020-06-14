ecd5e04218df0e13b1de37d68befaba218a33385
naman kumar <namank_11@outlook.com>
2020-06-14 22:12:43 +0530

    chapter_sixteen completed

--- chapter_sixteen/eq_explore_data.py
+++ chapter_sixteen/eq_explore_data.py
@@ -1,9 +1,28 @@
 import json
 
-file_name = 'data_json/eq_data_1_day_m1.json'
+file_name = 'data_json/eq_data_30_day_m1.json'
 
 with open(file_name) as f:
     all_eq_data = json.load(f)
-readable_file = 'data_json/readable_data.json'
-with open(readable_file, 'a') as f:
-    json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+meta_data_title = all_eq_data['metadata']['title']
+print(len(all_eq_dicts))
+
+hover_texts = []
+mags = []
+lons = []
+lats = []
+
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    title = eq_dict['properties']['title']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    hover_texts.append(title)
+print(mags[:10])
+print(lons[:10])
+print(lats[:10])
---
+++ chapter_sixteen/eq_world_map.py
@@ -0,0 +1,21 @@
+import chapter_sixteen.eq_explore_data
+
+import json
+
+from plotly.graph_objs import Scattergeo, Layout
+
+from plotly import offline
+
+from chapter_sixteen import eq_explore_data
+
+my_data = [Scattergeo(lon=eq_explore_data.lons, lat=eq_explore_data.lats, text=eq_explore_data.hover_texts, marker={
+        'size': [5*mag for mag in eq_explore_data.mags],
+        'color': eq_explore_data.mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
+    })]
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': my_data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
