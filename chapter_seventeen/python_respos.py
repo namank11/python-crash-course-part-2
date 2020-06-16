a87cb0efc62c01b98ea97ee424b77b228b0eac7b
naman kumar <namank_11@outlook.com>
2020-06-16 09:31:14 +0530

    chapter_seventeen files added

--- chapter_seventeen/python_respos.py
+++ chapter_seventeen/python_respos.py
@@ -14,6 +14,7 @@ print(response_dict.keys())
 print(f"Total repositories: {response_dict['total_count']}")
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
+repo_links, stars, labels = [], [], []
 print(f"Repositories returned: {len(repo_dicts)}")
 # Examine the first repository.
 repo_dict = repo_dicts[0]
@@ -22,6 +23,15 @@ for key in sorted(repo_dict.keys()):
     print(key)
 print("\nSelected information about each repository:")
 for repo_dict in repo_dicts:
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+    stars.append(repo_dict['stargazers_count'])
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
     print(f"Name: {repo_dict['name']}")
     print(f"Owner: {repo_dict['owner']['login']}")
     print(f"Stars: {repo_dict['stargazers_count']}")
@@ -30,3 +40,15 @@ for repo_dict in repo_dicts:
     print(f"Updated: {repo_dict['updated_at']}")
     print(f"Description: {repo_dict['description']}")
     print('')
+data = [Bar(x=repo_links, y=stars, text=labels, marker={
+    'color': 'rgb(60, 100, 150)',
+    'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
+    },
+    opacity=0.6)]
+my_layout = {
+'title': 'Most-Starred Python Projects on GitHub',
+'xaxis': {'title': 'Repository'},
+'yaxis': {'title': 'Stars'},
+}
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='python_repos.html')
